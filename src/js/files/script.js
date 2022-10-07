4a16525164205b11982bfb98a0e261ecb9a3e15d
sergei00026 <sergei57fx@mail.ru>
2022-10-07 22:50:40 +0500

    first commit

---
+++ src/js/files/script.js
@@ -0,0 +1,27 @@
+// Подключение функционала "Чертогов Фрилансера"
+import { isMobile } from "./functions.js";
+// Подключение списка активных модулей
+import { flsModules } from "./modules.js";
+
+const html = document.querySelector('html');
+const headerList = document.querySelector('.header__container');
+
+document.addEventListener("click", function (e) {
+	if (headerList.classList.contains('_active-menu') && !e.target.closest('.main-header__list')) {
+		html.classList.toggle('menu-open')
+		headerList.classList.toggle('_active-menu')
+	} else if (e.target.closest('.menu__icon')) {
+		html.classList.toggle('menu-open')
+		headerList.classList.toggle('_active-menu')
+	}
+});
+
+//run-line
+let runLine = document.querySelector('.page__runline');
+let text = runLine.innerHTML;
+function runStroke() {
+	text = text[text.length - 1] + text.substring(0, text.length - 1);
+	runLine.innerHTML = text;
+
+}
+setInterval(runStroke, 100);
